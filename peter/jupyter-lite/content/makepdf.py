201707df8b2052f1fdb561e89e0161006a54d0c7
Tobias Fischer <info@tobiasfischer.info>
2026-05-28 13:11:12 +1000

    Build Part 1 notebooks as PDFs

--- peter/jupyter-lite/content/makepdf.py
+++ peter/jupyter-lite/content/makepdf.py
@@ -1,4 +1,6 @@
+import re
 from pathlib import Path
+
 import nbformat
 from nbconvert.exporters import PDFExporter
 from nbconvert.preprocessors import ExecutePreprocessor
@@ -12,8 +14,54 @@ NOTEBOOKS = [
 
 EXPORT_ONLY_CODE = {
     "spatial-math.ipynb": "if NBCONVERT:\n    from spatialmath import BasePoseMatrix\n    BasePoseMatrix._color = False\n",
-    "robotics.ipynb": "if NBCONVERT:\n    from spatialmath import BasePoseMatrix\n    BasePoseMatrix._color = False\n",
+    "robotics.ipynb": """if NBCONVERT:
+    from spatialmath import BasePoseMatrix
+    BasePoseMatrix._color = False
+
+    # Avoid a Matplotlib/Robotics Toolbox colorbar issue in the conda-forge export stack.
+    from roboticstoolbox.mobile.PlannerBase import PlannerBase
+    _plot_bg = PlannerBase.plot_bg
+
+    def _plot_bg_without_colorbar(self, *args, **kwargs):
+        kwargs.setdefault("colorbar", False)
+        return _plot_bg(self, *args, **kwargs)
+
+    PlannerBase.plot_bg = _plot_bg_without_colorbar
+
+    import numpy as _np
+    from spatialmath import Polygon2
+    from roboticstoolbox.mobile.Animations import VehiclePolygon
+    _vehicle_polygon_init = VehiclePolygon.__init__
+
+    def _vehicle_polygon_init_compat(self, shape="car", scale=1, **kwargs):
+        coords = None
+        if isinstance(shape, Polygon2):
+            coords = shape.vertices()
+        elif isinstance(shape, _np.ndarray) and shape.ndim == 2:
+            if shape.shape[0] == 2:
+                coords = shape
+            elif shape.shape[1] == 2:
+                coords = shape.T
+
+        if coords is not None:
+            _vehicle_polygon_init(self, "car", scale=1, **kwargs)
+            self._coords = coords * scale
+            self._args = kwargs
+            return None
+
+        return _vehicle_polygon_init(self, shape, scale=scale, **kwargs)
+
+    VehiclePolygon.__init__ = _vehicle_polygon_init_compat
+""",
 }
+
+MARKDOWN_IMAGE_RE = re.compile(r"!\[[^\]]+\]\(([^)]+)\)")
+
+
+def strip_markdown_image_alt_text(cell):
+    if cell.cell_type == "markdown":
+        cell.source = MARKDOWN_IMAGE_RE.sub(r"![](\1)", cell.source)
+
 
 outdir = Path("_artifacts/part1-pdfs")
 outdir.mkdir(parents=True, exist_ok=True)
@@ -27,6 +75,13 @@ executor = ExecutePreprocessor(timeout=600, kernel_name="python3")
 for nb_path, title in NOTEBOOKS:
     nb_file = Path(nb_path)
     nb_dir = nb_file.parent
+    resource_paths = [
+        nb_dir,
+        nb_dir / "support",
+        nb_dir / "support/figs",
+        content_root / "support",
+        content_root / "support/figs",
+    ]
     with nb_file.open("r", encoding="utf-8") as f:
         nb = nbformat.read(f, as_version=4)
 
@@ -34,7 +89,16 @@ for nb_path, title in NOTEBOOKS:
     if len(nb.cells) >= 2:
         nb.cells = nb.cells[2:]
 
-    injected_cells = [nbformat.v4.new_code_cell("NBCONVERT = True\n")]
+    for cell in nb.cells:
+        strip_markdown_image_alt_text(cell)
+
+    injected_cells = [
+        nbformat.v4.new_code_cell(
+            "NBCONVERT = True\n"
+            "import warnings\n"
+            "warnings.filterwarnings('ignore')\n"
+        )
+    ]
 
     export_only_code = EXPORT_ONLY_CODE.get(nb_file.name)
     if export_only_code:
@@ -54,13 +118,7 @@ for nb_path, title in NOTEBOOKS:
             # Helps nbconvert resolve local assets referenced by notebook markdown.
             "path": str(nb_dir),
         },
-        "resource_paths": [
-            str(nb_dir),
-            str(nb_dir / "support"),
-            str(nb_dir / "support/figs"),
-            str(content_root / "support"),
-            str(content_root / "support/figs"),
-        ],
+        "resource_paths": [str(path) for path in resource_paths],
     }
 
     executor.preprocess(nb, resources)
